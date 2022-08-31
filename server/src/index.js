5c541212a4409fba9ea400fdc4fe78adc4877805
jixer's laptop <thewhiterabbit1994@outlook.com>
2022-08-31 13:26:57 +0430

    minor bug fix

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,7 @@ import '@lib/global'
 import runServer from '@lib/server/run-server'
 import buildSchema from 'graphql/schema/buildSchema'
 import resolvers from 'graphql/resolvers'
-import path from 'path'
-import { existsSync, mkdirSync } from 'fs'
 
-const dbDir = path.join(process.cwd(), '/src/db')
-
-if (!existsSync(dbDir)) {
-  mkdirSync(dbDir)
-}
 
 const typeDefs = buildSchema()
 
